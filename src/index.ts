1db5005fbff9d7592f21978497640de1ede01515
satou-yosiyuki <stuysyk1027@icloud.com>
2024-07-24 23:41:48 +0900

    ブランド型の練習

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import fastify from "fastify";
-import { Brand, CreateBrandType } from "./utils/createBrand";
+import { Brand, CreateBrandTypeValue } from "./utils/createBrand";
 
 type UserId = Brand<string, "UserId">;
-const userId: UserId = CreateBrandType<string, "UserId">("userId");
+const userId: UserId = CreateBrandTypeValue<string, "UserId">("userId");
 console.log("userId", userId);
+
+// ユーザ情報を定義する
+type UserInfo = { userId: string; name: string };
+// ブランド型のユーザ情報を定義する
+type BrandUserInfo = Brand<UserInfo, "UserInfo">;
+
+const userInfo: BrandUserInfo = CreateBrandTypeValue<
+  UserInfo, // 型を定義
+  "UserInfo" // 型名を定義
+>(
+  {
+    userId: "abcdefg",
+    name: "john",
+  } // 値を定義
+);
+console.log("userInfo", userInfo);
 
 const server = fastify();
 const opts = {
--- src/utils/createBrand.ts
+++ src/utils/createBrand.ts
@@ -1,6 +1,7 @@
+// ブランド型を定義する汎用的な型
 export type Brand<K, T> = K & { __brand: T };
 
-// 型を作る関数
-export const CreateBrandType = <K, T>(value: K): Brand<K, T> => {
+// ブランド型の値を作る汎用的な関数
+export const CreateBrandTypeValue = <K, T>(value: K): Brand<K, T> => {
   return value as Brand<K, T>;
 };
